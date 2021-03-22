f9a7bd5f147390c5b19103da26ceb66dbe1a7144
Ro4052 <Ro4052@users.noreply.github.com>
2021-03-22 17:19:52 +0000

    refactor Strategy code

--- src/com/designpatterns/strategy/Original/TeaMaker.java
+++ src/com/designpatterns/strategy/Original/TeaMaker.java
@@ -6,22 +6,20 @@ public class TeaMaker {
     }
 
     private void pourTea() {
-        System.out.println("Pouring tea");
+        System.out.println("Pouring tea...");
     }
 
     public void brewTea(String type) {
         boilWater();
-        if (type == "Breakfast") {
-            System.out.println("Brewing Breakfast tea...");
-        } else if (type == "Earl Grey") {
-            System.out.println("Brewing Earl Grey tea...");
-        } else if (type == "Matcha") {
-            System.out.println("Brewing Matcha tea...");
-        } else if (type == "Oolong") {
-            System.out.println("Brewing Oolong tea...");
-        } else {
-            System.out.println("I don't know what kind of tea that is!");
-            return;
+        switch (type) {
+            case "Breakfast" -> System.out.println("Brewing Breakfast tea...");
+            case "Earl Grey" -> System.out.println("Brewing Earl Grey tea...");
+            case "Matcha" -> System.out.println("Brewing Matcha tea...");
+            case "Oolong" -> System.out.println("Brewing Oolong tea...");
+            default -> {
+                System.out.println("I don't know what kind of tea that is!");
+                return;
+            }
         }
         pourTea();
     }
